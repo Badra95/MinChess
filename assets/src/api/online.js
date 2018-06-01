a673a28b6c3937a4553009e17b63c2deef03019a
Badra95 <Badra95100@gmail.com>
2018-06-01 10:18:00 +0200

    socket.io

--- assets/src/api/online.js
+++ assets/src/api/online.js
@@ -4,7 +4,7 @@ module.exports = (expressApp) => {
 
     var http = require('http').Server(app);
     var io = require('socket.io')(http);
-    var port = process.env.PORT || 3000;
+    var port = process.env.PORT || 3002;
 
     var lobbyUsers = {};
     var users = {};
